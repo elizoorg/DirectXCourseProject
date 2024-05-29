634d005d91f1cd5a7314dba28676a0e16a8bb5f9
Никита Елисеев <el1zoorg@yandex.ru>
2024-05-29 12:31:12 +0300

    Added classes to work with light

--- DirectXCourseProject/src/DirectionalLight.h
+++ DirectXCourseProject/src/DirectionalLight.h
@@ -16,13 +16,21 @@ class ENGINE_API DirectionalLight
     std::vector<float> shadowCascadeLevels_{};
 public:
     DirectionalLight(Engine::Application* g);
+
     DirectX::SimpleMath::Vector4 GetDirection() const { return lightDirection_; }
+
     void SetDirection(const DirectX::SimpleMath::Vector4 dir) { lightDirection_ = dir; }
+
     DirectX::SimpleMath::Vector4 GetColor() const { return lightColor_; }
+
     void SetColor(const DirectX::SimpleMath::Vector4 col) { lightColor_ = col; }
+
     DirectX::SimpleMath::Matrix GetLightSpaceMatrix(float nearPlane, float farPlane);
+
     DirectX::SimpleMath::Vector4 GetShadowCascadeDistances() const;
+
     std::vector<DirectX::SimpleMath::Matrix> GetLightSpaceMatrices();
+
 };
 
 
